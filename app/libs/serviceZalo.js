c576b3e28399c73e30c702cbcb9843eeae92c5ab
Đào Tuấn <ldemon1005@gmail.com>
2020-08-04 14:11:40 +0700

    tuan

--- app/libs/serviceZalo.js
+++ app/libs/serviceZalo.js
@@ -41,6 +41,7 @@ module.exports = {
             json: true
         };
         const {statusCode, body} = await request.postAsync(opts);
+        console.log("params", params)
         console.log('body:', body);
         return {
             body: body,
