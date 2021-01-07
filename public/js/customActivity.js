9f12fb3c7749fa3f0b4212a861b12a67c05b165f
ldemon <ldemon1005@gmail.com>
2021-01-07 15:59:50 +0700

    activity

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -9,8 +9,7 @@ define([
     var payload = {};
     var lastStepEnabled = false;
     var steps = [ // initialize to the same value as what's set in config.json for consistency
-        { "label": "Step 1", "key": "step1" },
-        { "label": "Step 2", "key": "step2" }
+        { "label": "Setup Zalo OA Channel", "key": "step1" }
     ];
     var currentStep = steps[0].key;
 
@@ -129,18 +128,6 @@ define([
                     visible: false
                 });
                 break;
-            case 'step2':
-                $('#step2').show();
-                connection.trigger('updateButton', {
-                    button: 'back',
-                    visible: true
-                });
-                connection.trigger('updateButton', {
-                    button: 'next',
-                    text: 'next',
-                    visible: true
-                });
-                break;
         }
     }
 
@@ -154,7 +141,7 @@ define([
         // Journey Builder sends an initial payload with defaults
         // set by this activity's config.json file.  Any property
         // may be overridden as desired.
-        payload.name = name;
+        payload.name = 'Zalo Message';
 
         payload['arguments'].execute.inArguments = [
             {"oa_id": oa_id},
